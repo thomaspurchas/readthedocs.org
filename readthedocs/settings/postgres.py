edc0b85fb22aa83997a497650007f435a5ede335
Thomas Purchas <tpurchas@gmail.com>
2011-08-24 10:47:25 +0100

    Changed the setting to mach local use.

--- readthedocs/settings/postgres.py
+++ readthedocs/settings/postgres.py
@@ -3,27 +3,38 @@ from .base import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'docs',
-        'USER': 'postgres',                      # Not used with sqlite3.
+        'NAME': 'readthedocs',
+        'USER': '',
         'PASSWORD': '',
-        'HOST': 'golem',
+        'HOST': '',
         'PORT': '',
     }
 }
 
+REDIS = {
+    'host': 'localhost',
+    'port': 6379,
+    'db': 0,
+}
+
 DEBUG = False
-TEMPLATE_DEBUG = False
+TEMPLATE_DEBUG = DEBUG
 CELERY_ALWAYS_EAGER = False
 
-MEDIA_URL = 'http://media.readthedocs.org/'
-ADMIN_MEDIA_PREFIX = MEDIA_URL + 'admin/'
-CACHE_BACKEND = 'memcached://localhost:11211/'
-SESSION_ENGINE = "django.contrib.sessions.backends.cached_db"
+MEDIA_URL = 'http://172.16.1.131/docs/media/'
+#ADMIN_MEDIA_PREFIX = MEDIA_URL + 'admin/'
+#CACHE_BACKEND = 'memcached://localhost:11211/'
+#SESSION_ENGINE = "django.contrib.sessions.backends.cached_db"
+SESSION_ENGINE = 'django.contrib.sessions.backends.file'
 
-HAYSTACK_SEARCH_ENGINE = 'solr'
-HAYSTACK_SOLR_URL = 'http://odin:8983/solr'
+#HAYSTACK_SEARCH_ENGINE = 'solr'
+#HAYSTACK_SOLR_URL = 'http://odin:8983/solr'
+
+CACHE_BACKEND = 'dummy://'
 
 
+import djcelery
+djcelery.setup_loader()
 
 try:
     from local_settings import *
