1bfcab178998b4897e389b4a99c80321f00f5684
Thomas Purchas <tpurchas@gmail.com>
2011-08-24 10:45:22 +0100

    Minor changes for debugging.

--- readthedocs/settings/sqlite.py
+++ readthedocs/settings/sqlite.py
@@ -9,16 +9,16 @@ DATABASES = {
                     'NAME': os.path.join(SITE_ROOT, 'dev.db'),
                 }
 }
-REDIS = {
-    'host': 'localhost',
-    'port': 6379,
-    'db': 0,
-}
+#REDIS = {
+#    'host': 'localhost',
+#    'port': 6379,
+#    'db': 0,
+#}
 
 
 
 SESSION_COOKIE_DOMAIN = None
-HAYSTACK_SOLR_URL = 'http://localhost:8983/solr'
+#HAYSTACK_SOLR_URL = 'http://localhost:8983/solr'
 CACHE_BACKEND = 'dummy://'
 
 TEST_RUNNER = 'xmlrunner.extra.djangotestrunner.XMLTestRunner'
